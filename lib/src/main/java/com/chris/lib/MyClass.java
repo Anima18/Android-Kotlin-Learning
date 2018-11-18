152301ef996ccf78673da55416c011ac26423d53
Anima18 <591151451@qq.com>
2018-11-18 23:12:23 +0800

    init

---
+++ lib/src/main/java/com/chris/lib/MyClass.java
@@ -0,0 +1,4 @@
+package com.chris.lib;
+
+public class MyClass {
+}
---
+++ lib/src/main/java/com/chris/lib/TestKotlin4.kt
@@ -0,0 +1,8 @@
+package com.chris.lib
+
+object TestKotlin4 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("ddddd2")
+    }
+}
