e8c155b2eb13d2278f28e7031508e54b0d5e5d51
Anima18 <591151451@qq.com>
2018-11-21 22:16:27 +0800

    渲染list数据

--- app/src/main/java/com/chris/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/chris/kotlindemo/MainActivity.kt
@@ -3,10 +3,17 @@ package com.chris.kotlindemo
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.net.URL
 
 
 class MainActivity : AppCompatActivity() {
+    val TAG: String = "KotlinDemo"
     private val items = listOf(
         "Mon 6/23 - Sunny - 31/17",
         "Tue 6/24 - Foggy - 21/8",
@@ -21,6 +28,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mainAct_recyclerView.layoutManager = LinearLayoutManager(this)
-        mainAct_recyclerView.adapter = RecyclerViewAdapter(items)
+
+        doAsync {
+            var data: List<Follower> = getRequestData("https://api.github.com/users/Anima18/followers")
+            uiThread{
+                Log.i(TAG, "running in mainThread")
+                mainAct_recyclerView.adapter = RecyclerViewAdapter(data)
+            }
+        }
+
     }
+
+    fun getRequestData(url: String): List<Follower> {
+        var data: String = URL(url).readText()
+        Log.i(TAG, data)
+        val followerType = genericType<List<Follower>>()
+        return Gson().fromJson<List<Follower>>(data, followerType)
+    }
+
+    inline fun <reified T> genericType() = object: TypeToken<T>() {}.type
 }
--- app/src/main/java/com/chris/kotlindemo/RecyclerViewAdapter.kt
+++ app/src/main/java/com/chris/kotlindemo/RecyclerViewAdapter.kt
@@ -1,13 +1,18 @@
 package com.chris.kotlindemo
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 
-class RecyclerViewAdapter(val items : List<String>) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+
+
+class RecyclerViewAdapter(val items : List<Follower>) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(TextView(parent.context))
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.listview_main_item, parent, false)
+        return ViewHolder(itemView)
     }
 
     override fun getItemCount(): Int {
@@ -15,8 +20,13 @@ class RecyclerViewAdapter(val items : List<String>) : RecyclerView.Adapter<Recyc
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.textView.text = items[position]
+        holder.textView.text = items[position].login
     }
 
-    class ViewHolder(val textView : TextView) : RecyclerView.ViewHolder(textView)
+    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        var textView: TextView
+        init {
+            textView = itemView.findViewById(R.id.mainList_title_tv)
+        }
+    }
 }
