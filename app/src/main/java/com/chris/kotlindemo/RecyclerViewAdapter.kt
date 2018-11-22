9abca3df07ecf32714d25c325bbe4195c0d05b92
Anima18 <591151451@qq.com>
2018-11-22 23:02:48 +0800

    learning lambda

--- app/src/main/java/com/chris/kotlindemo/RecyclerViewAdapter.kt
+++ app/src/main/java/com/chris/kotlindemo/RecyclerViewAdapter.kt
@@ -4,14 +4,21 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
-
+import com.squareup.picasso.Picasso
 
 
 class RecyclerViewAdapter(val items : List<Follower>) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
+    interface RecyclerOnClick {
+        fun onItemClick(view: View, position: Int)
+    }
+
+    var itemClick: RecyclerOnClick? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.listview_main_item, parent, false)
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.listview_two_line_with_avatar, parent, false)
         return ViewHolder(itemView)
     }
 
@@ -20,13 +27,22 @@ class RecyclerViewAdapter(val items : List<Follower>) : RecyclerView.Adapter<Rec
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.textView.text = items[position].login
+        with(items[position]) {
+            holder.textView.text = login
+            Picasso.get().load(avatar_url).into(holder.avatarView)
+        }
+
     }
 
-    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
-        var textView: TextView
+    inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        val avatarView: ImageView
+        val textView: TextView
         init {
-            textView = itemView.findViewById(R.id.mainList_title_tv)
+            avatarView = itemView.findViewById(R.id.list_item_avatar)
+            textView = itemView.findViewById(R.id.list_title_tv)
+
+            itemView.setOnClickListener({view -> itemClick!!.onItemClick(itemView, layoutPosition)})
+            //if(onItemClick != null) {}
         }
     }
 }
